ecb031c023cccf9408d7e9d33e0bb2ccff1d8239
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-10-15 16:48:53 -0400

    finishing authentication step

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+
 const cors = require('cors');
 
 const db = require('./database/dbConfig.js');
+
+const bcrypt = require('bcryptjs');
 
 const server = express();
 
@@ -12,10 +15,40 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
+server.post('/register', (req, res) => {
+  const credentials = req.body; 
+
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+
+  db('users').insert(credentials).then(ids => {
+    const id = ids[0];
+    res.status(201).json({ newUserId: id})
+  })
+   .catch(err => {
+     res.status(500).json(err);
+   });
+});
+
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users').where({username: creds.username}).first().then(user => {
+    if (user && bcrypt.compareSync(creds.password, user.password)) {
+      res.status(200).json({welcome: user.username})
+    }
+    else {
+      res.status(401).json({message: 'You just cannot enter. That is all.'})
+    }
+  })
+  .catch(err => req.status(500).json({ message: 'Something went wrong'}));
+
+})
+
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+server.get('/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
